60774f157d36714a30272d2c4b9f0826309a188d
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-02-04 14:14:49 +0100

    Added all pieces

--- src/constants/pieces.js
+++ src/constants/pieces.js
@@ -1,54 +1,260 @@
 const makeColour = () => `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`
 
+const makePiece = (piece) => ({
+  ...piece,
+  colour: makeColour()
+})
+
+function shuffle (array) {
+  for (let i=array.length - 1; i > 0; i--) {
+    let j=Math.floor(Math.random() * (i+1))
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 let pieces = []
 
-pieces.push({
+pieces.push(makePiece({
   shape: [[2,2,1],
           [0,1,0],
           [0,1,0]],
-  costTime: 5,
   costButtons: 5,
-  colour: makeColour()
-})
+  costTime: 5,
+}))
 
-pieces.push({
+pieces.push(makePiece({
+  shape: [[2,0,0,0],
+          [2,1,1,1],
+          [1,0,0,0]],
+  costButtons: 7,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,0,0,1],
+          [1,1,1,1],
+          [1,0,0,0]],
+  costButtons: 1,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,1,0,0],
+          [1,1,1,1],
+          [0,0,1,0]],
+  costButtons: 2,
+  costTime: 1,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,2,0],
+          [1,2,1],
+          [0,1,0]],
+  costButtons: 5,
+  costTime: 4,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,1,0,0],
+          [1,2,1,1],
+          [0,1,0,0]],
+  costButtons: 0,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,0,1,0,0],
+          [1,1,2,1,1],
+          [0,0,1,0,0]],
+  costButtons: 1,
+  costTime: 4,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,2,0],
+          [2,1,1],
+          [0,0,1]],
+  costButtons: 8,
+  costTime: 6,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,0,1],
+          [1,1,1],
+          [1,0,1]],
+  costButtons: 2,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,2,0],
+          [0,2,1],
+          [0,0,1]],
+  costButtons: 10,
+  costTime: 4,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,2,0],
+          [2,1,1],
+          [1,0,1]],
+  costButtons: 3,
+  costTime: 6,
+}))
+
+pieces.push(makePiece({
   shape: [[2,2],
           [1,1]],
-  costTime: 5,
   costButtons: 6,
-  colour: makeColour()
-})
+  costTime: 5,
+}))
 
-pieces.push({
+pieces.push(makePiece({
   shape: [[2,1,1,1,1]],
-  costTime: 1,
   costButtons: 7,
-  colour: makeColour()
-})
+  costTime: 1,
+}))
 
-pieces.push({
+pieces.push(makePiece({
+  shape: [[2,1,1,1]],
+  costButtons: 3,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,1,1]],
+  costButtons: 2,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
   shape: [[2,1,1,1],
           [1,0,0,1]],
-  costTime: 5,
   costButtons: 1,
-  colour: makeColour()
-})
+  costTime: 5,
+}))
 
-pieces.push({
+pieces.push(makePiece({
+  shape: [[0,0,2,0],
+          [1,1,1,1]],
+  costButtons: 3,
+  costTime: 4,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,2,2,0],
+          [1,1,1,1]],
+  costButtons: 7,
+  costTime: 4,
+}))
+
+pieces.push(makePiece({
+  shape: [[0,1,0],
+          [1,1,1]],
+  costButtons: 2,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,1,0],
+          [1,1,1]],
+  costButtons: 2,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,0,0,0],
+          [2,1,1,1]],
+  costButtons: 10,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,2,0,0],
+          [2,1,1,1]],
+  costButtons: 10,
+  costTime: 5,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,1,0],
+          [0,1,1]],
+  costButtons: 3,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,2,0],
+          [0,2,1]],
+  costButtons: 7,
+  costTime: 6,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,1,1,0],
+          [0,1,1,1]],
+  costButtons: 4,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[2,1,1,0],
+          [0,0,1,1]],
+  costButtons: 2,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,1,1],
+          [1,0,1]],
+  costButtons: 1,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
   shape: [[2,0,0],
           [2,1,1]],
-  costTime: 6,
   costButtons: 4,
-  colour: makeColour()
-})
+  costTime: 6,
+}))
 
-pieces.push({
+pieces.push(makePiece({
+  shape: [[2,0,0],
+          [1,1,1]],
+  costButtons: 4,
+  costTime: 2,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,0],
+          [1,1]],
+  costButtons: 1,
+  costTime: 3,
+}))
+
+pieces.push(makePiece({
+  shape: [[1,0],
+          [1,1]],
+  costButtons: 3,
+  costTime: 1,
+}))
+
+pieces.push(makePiece({
   shape: [[0,2,1,0],
           [1,1,1,1],
           [0,1,1,0]],
-  costTime: 5,
-  costButtons: 3,
-  colour: makeColour()
-})
+  costButtons: 5,
+  costTime: 3,
+}))
+
+shuffle(pieces);
+
+pieces.push(makePiece({
+  shape: [[1,1]],
+  costButtons: 2,
+  costTime: 1,
+}))
 
 export default pieces
