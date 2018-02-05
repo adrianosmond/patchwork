2ef5957dca00850715495079bc82f1fca4278923
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-02-05 16:50:03 +0100

    Instructions update - included link to youtube game explanation

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Patchwork
 
-A version of Uwe Rosenberg's two-player board game "Patchwork"
+This is a version of the Uwe Rosenberg two-player board game Patchwork. If you need to know how to play the game, you can find ([a full explanation of the rules](https://www.youtube.com/watch?v=wo7QGN2u3a0&t=2m12s/)) at this link
 
 ## Controls
 
-- Move pieces with the arrow keys
-- Rotate pieces with the space bar
-- Flip pieces by hoding shift and pressing the space bar
-- Place pieces with the return key
+- Click on a piece to get it on your board
+- Use the arrow keys to move it around the board
+- Use the space bar to rotate it
+- Hold shift and use the space bar to flip it
+- Use enter key to permanently place it on the board
+
+Touch screen controls are available by tapping the button in the bottom right of the screen. The game should work on tablets but hasn't been optimised for mobile sized screens.
--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -40,17 +40,20 @@ class Instructions extends Component {
         <div className="instructions__modal">
           <h1 className="instructions__modal-heading">Patchwork</h1>
           <h2>Rules</h2>
-          <p>This is a version of the Uwe Rosenberg two-player boardgame Patchwork.
-            If you need to know how to play the game, at this link you can
-            find <a href="" target="_blank">a full explanation of the rules</a></p>
+          <p>This is a version of the Uwe Rosenberg two-player board game Patchwork.
+            If you need to know how to play the game, you can find
+            find <a href="https://www.youtube.com/watch?v=wo7QGN2u3a0&t=2m12s" rel="noopener noreferrer" target="_blank">a
+            full explanation of the rules</a> at this link</p>
           <h2>Controls</h2>
           <ul>
             <li>Click on a piece to get it on your board</li>
             <li>Use the arrow keys to move it around the board</li>
             <li>Use the space bar to rotate it</li>
-            <li>Use shift and the space bar to flip it</li>
+            <li>Hold shift and use the space bar to flip it</li>
             <li>Use enter key to permanently place it on the board</li>
           </ul>
+          <p>If you're using a tablet, you can bring up touch screen controls to use
+            instead of a keyboard by tapping the button in the bottom right corner</p>
           <div className="instructions__modal-controls">
             <label><input type="checkbox" ref={(el)=>{this.neverShowCheckbox = el}} />Don't show me this again</label>
             <button className="instructions__modal-button" onClick={this.closeModal.bind(this)}>Got it!</button>
